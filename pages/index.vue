531bcbd0027b1460123ed0b8d77d0d74fbd35716
Julen <julen@folkytools.com>
2020-02-11 01:07:06 +0100

    Add link to change language reproducing the error

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="container">
-    <ProductZoomer :base-images="images" :base-zoomer-options="zoomerOptions"></ProductZoomer>
-  </div>
+    <div class="container">
+        <ProductZoomer :base-images="images" :base-zoomer-options="zoomerOptions"></ProductZoomer>
+        <h1>{{ $t('greeting') }}</h1>
+
+        <nuxt-link v-if="$i18n.locale !== 'en'" :to="switchLocalePath('en')">English</nuxt-link>
+
+        <nuxt-link v-if="$i18n.locale !== 'es'" :to="switchLocalePath('es')">Español</nuxt-link>
+    </div>
 </template>
 
 <script>
